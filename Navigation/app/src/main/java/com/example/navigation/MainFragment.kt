1bfe18f7e29585f7566cb6440c356a947e57299c
Anbyeonghun <abh0920one@gmail.com>
2021-04-22 14:44:10 +0900

    Navigation

---
+++ Navigation/app/src/main/java/com/example/navigation/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.example.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import kotlinx.android.synthetic.main.fragment_main.*
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainFragment : Fragment() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener {
+
+            val action =  MainFragmentDirections.actionMainFragmentToSecondFragment("Hello!")
+            findNavController().navigate(action)
+        }
+    }
+}
---
+++ Navigation/app/src/main/java/com/example/navigation/SecondFragment.kt
@@ -0,0 +1,32 @@
+package com.example.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavArgs
+import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_second.*
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SecondFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SecondFragment : Fragment() {
+
+    val args: SecondFragmentArgs by navArgs()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        textView.text = args.text
+    }
+}
